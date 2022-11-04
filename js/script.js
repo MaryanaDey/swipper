a1d3a1c2fb44e7c3810f2522032d6aef49a3223a
Mariana Dei <dey.maryana1989@gmail.com>
2022-11-04 16:43:18 +0100

    add hashnavigation

--- js/script.js
+++ js/script.js
@@ -18,16 +18,44 @@ new Swiper('.image-slider', {
             return '<span class="' + className + '">' + (index + 1) + '</span>';
         },
         */
-        /*
+
         //Фракция
         type: 'fraction',
         //Кастомный вывод фракции
         renderFraction: function (currentClass, totalClass) {
             return 'Фото <span class="' + currentClass + '"></span>' + 'из' + '<span class="' +
                 totalClass + '"></span>';
-    },
-        */
+        },
+
+        /*
         //Прогрессбар
         type: 'progressbar'
+        */
     },
+    //Скролл
+    scrollbar: {
+        el: '.swiper-scrollbar',
+        //Вощможность перетаскивать скролл
+        draggable: true
+    },
+
+    //включение/отклбчение
+    //перетаскивание на ПК
+    simulateTouch: true,
+    //чувствительность скролла
+    touchRadio: 1,
+    //Угол срабатывания свайпа/перетаскивания
+    touchAngle: 45,
+    //Курсор перетаскивания
+    grabCursor: true,
+
+    //Переклбчение при клике на слайд
+    slideToCliledsSlide: true,
+
+    //Навигация по хешу
+    hashNavigation: {
+        //Отслеживать состояние
+        watchState: true,
+    }
+
 });
